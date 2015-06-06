49010f7014979953cfe3d581f6b479d6deebdb9e
Alper Gungormusler <alpynaq@gmail.com>
2015-06-06 21:47:31 +0100

    Last commit missing diff added.

--- Assets/barelyMusician/Scripts/Composition/Base/Generation/MacroGenerator.cs
+++ Assets/barelyMusician/Scripts/Composition/Base/Generation/MacroGenerator.cs
@@ -81,5 +81,7 @@ namespace BarelyAPI
 
     public enum SectionType
     {
+        INTRO = 'I', VERSE = 'V', PRE_CHORUS = 'P', CHORUS = 'C', BRIDGE = 'B', OUTRO = 'O',
+        END = '.', NONE = ' '
     }
 }
