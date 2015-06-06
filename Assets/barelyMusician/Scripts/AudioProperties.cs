bdd39aa9be680ca9bd5d7f1fa46bc368e18f8378
Alper Gungormusler <alpynaq@gmail.com>
2015-06-06 21:28:29 +0100

    AudioProperties code style reverted to MSVS.

--- Assets/barelyMusician/Scripts/AudioProperties.cs
+++ Assets/barelyMusician/Scripts/AudioProperties.cs
@@ -8,20 +8,21 @@
 using UnityEngine;
 using System.Collections;
 
-namespace BarelyAPI {
+namespace BarelyAPI
+{
+    public static class AudioProperties
+    {
+        // System sampling rate.
+        public static int SampleRate;
 
-  public static class AudioProperties {
-    // System sampling rate.
-    public static int SampleRate;
-    
-    // System sampling interval (1 / SampleRate).
-    public static float Interval;
+        // System sampling interval (1 / SampleRate).
+        public static float Interval;
 
-    static AudioProperties() {
-      AudioConfiguration config = AudioSettings.GetConfiguration();
-      SampleRate = config.sampleRate;
-      Interval = 1.0f / SampleRate;
+        static AudioProperties()
+        {
+            AudioConfiguration config = AudioSettings.GetConfiguration();
+            SampleRate = config.sampleRate;
+            Interval = 1.0f / SampleRate;
+        }
     }
-  }
-
 }
